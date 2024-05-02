e749db495574c0dd002cbea73d2419201ec3e55d
Ahmad Abdulmajid <ahmad0abdulmajid@gmail.com>
2024-05-03 01:09:44 +0300

    add code in files types: ,.feature,.ts,.json remove code in files types: ,.json

--- content.py
+++ content.py
@@ -1,13 +1,24 @@
-# Contributions-Importer-For-Github/run_script.py
 import git
-from git_contributions_importer import *
+import os
+from app.src import Importer
 
-# Your private repo or Bitbucket repo
-repo = git.Repo("https://Tirador@bitbucket.org/xsonardivedevteamenv/atip-common.git")
-# Your mock repo
-mock_repo = git.Repo("https://github.com/Tirador1/mock-repo.git")
+# Define repository URLs and local paths
+private_repo_url = "https://Tirador@bitbucket.org/xsonardivedevteamenv/tests.git"
+mock_repo_url = "https://github.com/Tirador1/mock-repo.git"
+local_private_repo_path = "private_repo"
+local_mock_repo_path = "mock_repo"
+
+# Clone the repositories if they don't already exist
+if not os.path.exists(local_private_repo_path):
+    git.Repo.clone_from(private_repo_url, local_private_repo_path)
+if not os.path.exists(local_mock_repo_path):
+    git.Repo.clone_from(mock_repo_url, local_mock_repo_path)
+
+# Initialize the Repo objects with the local paths
+repo = git.Repo(local_private_repo_path)
+mock_repo = git.Repo(local_mock_repo_path)
+
+# Initialize the Importer and set the author
 importer = Importer([repo], mock_repo)
-# I use both my personal email and work email here,
-# Since the private repo uses work email, and Github uses my personal email
-importer.set_author(['ahmad0abdulmajid@gamil.com'])
+importer.set_author(['ahmad0abdulmajid@gmail.com'])  # Fixed email typo
 importer.import_repository()
